c7e3e0b4c8a17b684da00f6a975a8afdd5ca00ac
Kamran <kamran.personal@icloud.com>
2025-12-25 01:50:14 +0100

    feat: nix

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = [
+            pkgs.nixfmt
+
+            pkgs.cargo
+            pkgs.clippy
+            pkgs.rustc
+            pkgs.rustfmt
+            pkgs.rust-analyzer
+          ];
+
+          shellHook = ''
+            echo 'Entered dev shell..'
+            export RUST_LOG=debug
+            alias cr='cargo run'
+          '';
+        };
+      }
+    );
+}
